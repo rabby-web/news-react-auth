ec155a4109414eba19e57c64e007f3d5bd05018b
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-03 19:29:53 +0600

    react dragon news auth

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5t8sldRyoEzuicQC5GxlqUtopigFDrWY",
-  authDomain: "react-dragon-news-auth.firebaseapp.com",
-  projectId: "react-dragon-news-auth",
-  storageBucket: "react-dragon-news-auth.appspot.com",
-  messagingSenderId: "750044483429",
-  appId: "1:750044483429:web:77f4f5d3f33234b21f5b8c"
+  apiKey: "AIzaSyDNoz2Z0jUFfqi1mW8vcUuM923ZBMc-Qnc",
+  authDomain: "news-react-auth.firebaseapp.com",
+  projectId: "news-react-auth",
+  storageBucket: "news-react-auth.appspot.com",
+  messagingSenderId: "264533004417",
+  appId: "1:264533004417:web:313530bf5d398fb532fd80",
 };
 
 // Initialize Firebase
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
 
-
 const Root = () => {
-    return (
-        <div className="max-w-6xl mx-auto font-poppins">
-            <Outlet></Outlet>      
-        </div>
-    );
+  return (
+    <div className="max-w-7xl mx-auto font-poppins">
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Root;
