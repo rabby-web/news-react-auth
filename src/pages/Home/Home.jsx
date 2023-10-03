33fdd942fac8fc6249548fee2db177a32390ca65
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-03 22:35:00 +0600

    show news UI

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,33 @@
+import { useLoaderData } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 import BreakingNews from "./BreakingNews";
-
+import NewsCart from "../NewsCart/NewsCart";
 
 const Home = () => {
-    return (
-        <div>
-            <Header></Header>
-            <BreakingNews></BreakingNews>
-            <Navbar></Navbar>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-                <div className="border">
-                    <LeftSideNav></LeftSideNav>
-                </div>
-                <div className="md:col-span-2 border">
-                    <h2 className="text-4xl">News coming soon...</h2>
-                </div>
-                <div className="border">
-                    <RightSideNav></RightSideNav>
-                </div>
-            </div>
+  const allNews = useLoaderData();
+  return (
+    <div>
+      <Header></Header>
+      <BreakingNews></BreakingNews>
+      <Navbar></Navbar>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+        <div className="">
+          <LeftSideNav></LeftSideNav>
         </div>
-    );
+        <div className="md:col-span-2 ">
+          {allNews.map((news) => (
+            <NewsCart key={news._id} news={news}></NewsCart>
+          ))}
+        </div>
+        <div className="">
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/NewsCart/NewsCart.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+
+const NewsCart = ({ news }) => {
+  const { title, image_url, details, _id } = news || {};
+  return (
+    <div>
+      <div className="m-2">
+        <div className="card w-full p-2 glass">
+          <figure>
+            <img src={image_url} alt="car!" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{title}</h2>
+            {details.length > 200 ? (
+              <p>
+                {details.slice(0, 200)}{" "}
+                <Link to={`/news/${_id}`} className="text-blue-600">
+                  Read More...
+                </Link>
+              </p>
+            ) : (
+              <p>{details}</p>
+            )}
+            <div className="card-actions justify-end">
+              <Link to={`/news/${_id}`}>
+                <button className="btn btn-primary">View Details</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsCart;
